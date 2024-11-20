8fafcbf4f44b35964fcfd929cbfbb265d50d3ec7
cmcdev <cmcdev@vt.edu>
2024-11-20 16:15:55 -0500

    Added more to the python internal doc

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -4,7 +4,7 @@ from typing import List
 import requests
 
 
-
+#TODO maybe instead of file_in_name we could pass in the dataframe itself depending on the use case
 def generate_distance_matrix_curl(file_in_name: str,key: str,**kwargs)->List[List[float]]:
     """
 
@@ -12,6 +12,7 @@ def generate_distance_matrix_curl(file_in_name: str,key: str,**kwargs)->List[Lis
 
     Args:
         file_in_name: file name of the csv file
+        key: google api key
     
     Returns:
         List[List[float]]: distance matrix in km i'th and j'th element is the distance between i'th and j'th point travelled via road in km
@@ -22,6 +23,7 @@ def generate_distance_matrix_curl(file_in_name: str,key: str,**kwargs)->List[Lis
     distanceMatrix = []
     timeMatrix = []
     df = pd.read_csv(file_in_name)
+
     for i in range(len(df)):
         distanceMatrix.append([])
         timeMatrix.append([])
@@ -37,12 +39,10 @@ def generate_distance_matrix_curl(file_in_name: str,key: str,**kwargs)->List[Lis
                 distanceMatrix[i].append(response.json()['rows'][0]['elements'][k-j]['distance']['value']/1000)
                 timeMatrix[i].append(response.json()['rows'][0]['elements'][k-j]['duration']['value']/60)
        
- 
+    
     if 'distanceFile' in kwargs:
         pd.DataFrame(distanceMatrix).to_csv(kwargs['distanceFile'], index=False,header=False)
         
-
-  
     if 'timeFile' in kwargs:
         pd.DataFrame(timeMatrix).to_csv(kwargs['timeFile'], index=False,header=False)
         
@@ -58,6 +58,5 @@ if __name__ == "__main__":
         input_file = config['inputFile']
         outputDistFile = config['outputDistFile']
         outputTimeFile = config['outputTimeFile']
-        
-        
+
     generate_distance_matrix_curl("data.csv", key, distanceFile = outputDistFile, timeFile = outputTimeFile)
